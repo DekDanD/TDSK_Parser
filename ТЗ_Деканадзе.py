85b971c8fd9f8b5c32a90e8c71a8ce5ab2870e00
Деканадзе Даниил Давидович <dekanadze2103@mail.ru>
2026-05-01 21:17:03 +0500

    корректность заполнения

--- ТЗ_Деканадзе.py
+++ ТЗ_Деканадзе.py
@@ -8,15 +8,16 @@ def parse_apartments_csv():
     df = pd.read_csv('Экспозиция ТДСК с 01.07.2023 по 31.12.2023 (1).csv', sep='\t')
     actual_table = pd.DataFrame(columns=['Дата', 'Корпус', 'Количество активных квартир'])
 
-    df['actualized_at'] = pd.to_datetime(df['actualized_at'], format='ISO8601')
-    df['actualized_at'] = df['actualized_at'].dt.strftime('%d.%m.%y')
+    df['actualized_at'] = pd.to_datetime(df['actualized_at'], format='ISO8601').dt.tz_localize(None).dt.normalize()
     dates = pd.date_range(start='2023-07-01', end='2023-12-31')
-    dates_list = dates.strftime('%d.%m.%y').tolist()
     df['gp'] = [gp[:gp.find(')') + 1] for gp in df['address']]
     corpuses = df['gp'].unique()[df['gp'].unique() != '']
-    for i in dates_list:
+    rows = []
+    for i in dates:
         for gp in corpuses:
-            actual_table.loc[len(actual_table)] = {'Дата': i, 'Корпус': gp, 'Количество активных квартир': len(df[(df['actualized_at'] <= i) & (df['gp'] == gp)])}
+            rows.append({'Дата': i, 'Корпус': gp, 'Количество активных квартир': len(df[(df['actualized_at'] >= i) & (df['gp'] == gp)])})
 
-    actual_table.to_excel('actual_table.xlsx')
+    actual_table = pd.DataFrame(rows)
+
+    actual_table.to_excel('actual_table.xlsx', index=False)
 parse_apartments_csv()
