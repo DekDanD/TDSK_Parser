c0b955fa3c928bed94efbe4cafaa804b01c2cdd9
Деканадзе Даниил Давидович <dekanadze2103@mail.ru>
2026-05-01 19:39:52 +0500

    заполнение и запись таблицы

--- ТЗ_Деканадзе.py
+++ ТЗ_Деканадзе.py
@@ -6,19 +6,17 @@ import openpyxl
 
 def parse_apartments_csv():
     df = pd.read_csv('Экспозиция ТДСК с 01.07.2023 по 31.12.2023 (1).csv', sep='\t')
-    actual_table = pd.DataFrame()
-    df['actualized_at'] = pd.to_datetime(df['actualized_at'], format='ISO8601')
-    df['actualized_at'] = df['actualized_at'].dt.strftime('%d-%m-%Y')
-    print(df['actualized_at'].drop_duplicates())
-    dates = pd.date_range(start='2023-07-01', end='2023-12-31')
-    dates_list = dates.strftime('%d-%m-%y').tolist()
-    df['gp'] = [gp[:gp.find(')') + 1] for gp in df['address']]
-    print(df['gp'])
-    for i in dates_list:
-        actual_table.loc[len(actual_table)] = {'Дата': i}
-    for i, row in df.iterrows():
-        dates = pd.date_range(start='2023-07-01', end='2023-12-31')
-        
-    print(df)
+    actual_table = pd.DataFrame(columns=['Дата', 'Корпус', 'Количество активных квартир'])
 
+    df['actualized_at'] = pd.to_datetime(df['actualized_at'], format='ISO8601')
+    df['actualized_at'] = df['actualized_at'].dt.strftime('%d.%m.%y')
+    dates = pd.date_range(start='2023-07-01', end='2023-12-31')
+    dates_list = dates.strftime('%d.%m.%y').tolist()
+    df['gp'] = [gp[:gp.find(')') + 1] for gp in df['address']]
+    corpuses = df['gp'].unique()[df['gp'].unique() != '']
+    for i in dates_list:
+        for gp in corpuses:
+            actual_table.loc[len(actual_table)] = {'Дата': i, 'Корпус': gp, 'Количество активных квартир': len(df[(df['actualized_at'] <= i) & (df['gp'] == gp)])}
+
+    actual_table.to_excel('actual_table.xlsx')
 parse_apartments_csv()
